4fc18f7723e7c50612970fc7a47747fb9df6c5b8
Christopher Romano <christopher@romano.im>
2023-03-27 17:10:57 +0300

    Part 3: finished 3.5, 3.6 phonebookbackend (Issue #1)

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 let persons = [
   {
     id: 1,
@@ -23,6 +25,40 @@ let persons = [
     number: "39-23-6423122",
   },
 ];
+
+const generateId = () => {
+  return Math.floor(Math.random() * 100000000);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const duplicate = persons.find((person) => person.name === body.name);
+
+  if (duplicate) {
+    return response.status(409).json({
+      error: "name already exists",
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  console.log(persons);
+
+  response.json(person);
+});
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
